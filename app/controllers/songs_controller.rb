cf1d7f5650475d539e832097221e6150e4b174d9
“mpallenjr”git config --global user.email “mpallenjnr@gmail.com” <mikeallen@Michaels-MBP.attlocal.net>
2021-10-18 18:56:16 -0500

    complete restful routes

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,43 @@
 class SongsController < ApplicationController
+  def index
+    all_songs = Song.all
+    render json: all_songs.as_json
+  end
+
+  def create
+    new_song = Song.new(
+
+      title: params[:title],
+      artist: params[:artist],
+      album: params[:album],
+      year: params[:year],
+    )
+    new_song.save
+    render json: new_song.as_json
+  end
+
+  def show
+    the_id = params[:id]
+    show_song = Song.find_by(id: the_id)
+    render json: show_song.as_json
+  end
+
+  def update
+    the_id = params[:id]
+    update_song = Song.find_by(id: the_id)
+    update_song.title = params[:title]
+    update_song.artist = params[:artist]
+    update_song.album = params[:album]
+    update_song.year = params[:year]
+    update_song.save
+    render json: update_song.as_json
+  end
+
+  def delete
+    the_id = params[:id]
+    delete_song = Song.find_by(id: the_id)
+    delete_song.destroy
+    render json: {message: "Your song has been deleted"}
+  
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "/songs" => "songs#index"
+  post "/songs" => "songs#create"
+  get "/songs/:id" => "songs#show"
+  patch "/songs/:id" => "songs#update"
+  delete "/songs/:id" => "songs#delete"
 end
